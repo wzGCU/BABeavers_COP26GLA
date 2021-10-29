54939922344b6ff1e4f60c4df3fe83d3e21de0b3
wzGCU <61721211+wzGCU@users.noreply.github.com>
2021-10-29 15:41:30 +0100

    Camera Movement added yo

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMover : MonoBehaviour
+public class CameraMover : MonoBehaviour
 {
     // Update is called once per frame, Fixed makes it looks less laggy
     void FixedUpdate()
     {
         float horizontalX = Input.GetAxis("Horizontal"); //Gets the horizontal positive/negative axis value
-        float horizontalZ = Input.GetAxis("Vertical"); //Gets the vertical axis value
 
-        Vector3 movement = new Vector3(horizontalX, 0, horizontalZ);
+        Vector3 movement = new Vector3(horizontalX, 0, 0);
 
         transform.position += movement * Time.deltaTime;
     }
