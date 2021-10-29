21ec5f86226efa33085803f926bed3b55f9ed26b
wzGCU <61721211+wzGCU@users.noreply.github.com>
2021-10-29 20:59:30 +0100

    Tetris and blocks

---
+++ Assets/SpawnTetris.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetris : MonoBehaviour
+
+    
+{
+    public GameObject[] Tetrominoes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetromino();
+    }
+
+    // Update is called once per frame
+    public void NewTetromino()
+    {
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, transform.rotation);
+    }
+}
